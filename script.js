90e35bd596ff8260e9a13ac21cf8ba53d3c3852f
Jose1404 <cerqueira.7946@aluno.pr.senac.br>
2024-07-17 23:06:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-                                                                                                                                                                            
+                                                                                                                                                                            function redirectToPage(event) {
+    event.preventDefault();
+    window.location.href = "indexregistro.html";
+}
+
+
+function validateForm(event) {
+    event.preventDefault();
+
+    const password = document.getElementById('isenha').value;
+    const confirmPassword = document.getElementById('isenha_confirm').value;
+
+    if (password !== confirmPassword) {
+        alert('As senhas não coincidem.');
+        return false;
+    }
+
+    window.location.href = "index.html"; 
+    return true;
+}
