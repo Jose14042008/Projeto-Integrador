18d86bcef97adaf66f655668d7c22ac74b097fbd
Jose1404 <cerqueira.7946@aluno.pr.senac.br>
2024-08-05 23:09:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-    function redirecionarpágina(event) {
-    event.preventDefault();
-    window.location.href = "indexregistro.html";
-}
-function confirmarsenha(event) {
-    event.preventDefault();
-
-    const password = document.getElementById('isenha').value;
-    const confirmPassword = document.getElementById('isenha_confirm').value;
-
-    if (password !== confirmPassword) {
-        alert('As senhas não coincidem.');
-        return false;
-    }
-
-    window.location.href = "index.html"; 
-    return true;
-}
-
 document.getElementById('Tipoproduto').addEventListener('change', function() {
     const cardContainer = document.getElementById('cardcardgrande');
     cardContainer.innerHTML = '';
